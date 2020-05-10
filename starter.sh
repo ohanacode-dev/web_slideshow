8f697210f109aecc53ce95a3b52fbfe2d3784c7a
Rada Berar <rada.berar@ohanacode-dev.com>
2020-05-10 11:31:43 +0200

    Add updater

--- starter.sh
+++ starter.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#sleep 10
+curdir=$(dirname "$0")
+cd /tmp
+git clone git@github.com:ohanacode-dev/web_slideshow.git
+rsync -r web_slideshow ~
+
 gunicorn -b 0.0.0.0:8000 -w 3 display:app
 
 
--- web_launch.sh
+++ web_launch.sh
@@ -2,11 +2,9 @@
 
 cd $(dirname "$0")
 ./starter.sh &
-
 firefox -url http://localhost:8000 &
-
+xdotool mousemove_relative 2000 0
 sleep 5
-
 xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11
 
 
